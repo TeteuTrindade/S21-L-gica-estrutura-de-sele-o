9b1c86b12cc32bd1b23bfac299336a2ca0ff82e9
TeteuTrindade <teteutrindade307@gmail.com>
2024-09-12 16:32:08 -0300

    Add files via upload

---
+++ repeticao_for.py
@@ -0,0 +1,2 @@
+for i in range(5): # Para a variável i, está diponivel uma range(espaço) de valor 5.
+    print(f"Valor: {i}") # Aparecerá a quantidade de ranges.
---
+++ repeticao_while.py
@@ -0,0 +1,4 @@
+contador = 0 # O contador começará com 0.
+while contador < 5: # Enquanto o valor for menor que 5.
+    print(f"Contador: {contador}") # O contador aparecerá no terminal.
+    contador += 1 # E voltará acrecentando mais 1.
